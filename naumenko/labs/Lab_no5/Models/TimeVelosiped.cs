6bbef9750800e226eabd8e5f810145ac670dd7a1
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2020-10-16 21:50:58 +0300

    5,6,add5

---
+++ naumenko/labs/Lab_no5/Models/TimeVelosiped.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Lab_no5.Exceptions;
+
+namespace Lab_no5.Models
+{
+    class TimeVelosiped
+    {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+        private DateTime _timeCorrection;
+        public TimeVelosiped()
+        {
+            _timeCorrection = new DateTime(2020,10,5,0,0,0);
+        }
+
+        public TimeVelosiped(int hours, int minutes, int seconds)
+        {
+            SetTime(hours, minutes, seconds);
+            _timeCorrection = new DateTime(2020,10,5,0,0,0);
+            _timeCorrection = _timeCorrection.AddHours(Hours);
+            _timeCorrection = _timeCorrection.AddMinutes(Minutes);
+            _timeCorrection = _timeCorrection.AddSeconds(Seconds);
+        }
+
+        public override string ToString()
+        {
+            return $"{Hours}:{Minutes}:{Seconds}";
+        }
+
+        public int Hours
+        {
+            get => _hours;
+            set
+            {
+                if (value > 23 || value < 0)
+                {
+                    _hours = value / 23;
+                    throw new TimeHourException();
+                }
+                else
+                {
+                    _hours = value;
+                }
+                    
+                
+                _timeCorrection = new DateTime(2020, 10, 5, Hours, Minutes, Seconds);
+            }
+        }
+
+        public void AddSeconds(int seconds)
+        {
+            var time = _timeCorrection.AddSeconds(seconds);
+            Hours = time.Hour;
+            Minutes = time.Minute;
+            Seconds = time.Second;
+            _timeCorrection = time;
+        }
+
+        public void AddMinutes(int minutes)
+        {
+            var time = _timeCorrection.AddMinutes(minutes);
+            Hours = time.Hour;
+            Minutes = time.Minute;
+            Seconds = time.Second;
+            _timeCorrection = time;
+        }
+
+        public void AddHours(int hours)
+        {
+            var time = _timeCorrection.AddHours(hours);
+            Hours = time.Hour;
+            Minutes = time.Minute;
+            Seconds = time.Second;
+            _timeCorrection = time;
+        }
+
+        public int Minutes
+        {
+            get => _minutes;
+            set
+            {
+                if (value > 59 || value < 0)
+                {
+                    _minutes = value % 59;
+                    throw new TimeMinuteException();
+                }
+                else
+                {
+                    _minutes = value;
+                }
+                    
+                
+                _timeCorrection = new DateTime(2020, 10, 5, Hours, Minutes, Seconds);
+            }
+        }
+
+        public int Seconds
+        {
+            get => _seconds;
+            set
+            {
+                if (value > 59 || value < 0)
+                {
+                    _seconds = value % 59;
+                    throw new TimeSecondException();
+                }
+                else
+                {
+                    _seconds = value;
+                }
+                _timeCorrection = new DateTime(2020, 10, 5, Hours, Minutes, Seconds);
+            }
+        }
+
+        public void SetTime(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+    }
+}
---
+++ naumenko/labs/Lab_no5/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Lab_no5.Exceptions;
+using Lab_no5.Models;
+
+namespace Lab_no5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeVelosiped time = new TimeVelosiped();
+            TypeSeconds_Exception(time);
+            TypeMinutes_Exception(time);
+            TypeHours_Exception(time);
+            Console.WriteLine(time);
+
+            AddHours_test(time);
+            AddMinutes_test(time);
+            AddSeconds_test(time);
+
+            Console.WriteLine(time);
+            Console.ReadLine();
+        }
+
+        static void AddSeconds_test(TimeVelosiped time)
+        {
+            Console.WriteLine("Введите секунды (add)");
+            if (int.TryParse(Console.ReadLine(), out int seconds))
+            {
+                time.AddSeconds(seconds);
+            }
+        }
+
+        static void AddMinutes_test(TimeVelosiped time)
+        {
+            Console.WriteLine("Введите минуты (add)");
+            if (int.TryParse(Console.ReadLine(), out int min))
+            {
+                time.AddMinutes(min);
+            }
+        }
+
+        static void AddHours_test(TimeVelosiped time)
+        {
+            Console.WriteLine("Введите часы (add)");
+            if (int.TryParse(Console.ReadLine(), out int hours))
+            {
+                time.AddHours(hours);
+            }
+        }
+
+        static void TypeSeconds_Exception(TimeVelosiped time)
+        {
+            Console.WriteLine("Введите секунды (set)");
+            if (int.TryParse(Console.ReadLine(), out int seconds))
+            {
+                try
+                {
+                    time.Seconds = seconds;
+                }
+                catch (TimeSecondException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+
+        static void TypeMinutes_Exception(TimeVelosiped time)
+        {
+            Console.WriteLine("Введите минуты (set)");
+            if (int.TryParse(Console.ReadLine(), out int min))
+            {
+                try
+                {
+                    time.Minutes = min;
+                }
+                catch (TimeMinuteException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+
+        static void TypeHours_Exception(TimeVelosiped time)
+        {
+            Console.WriteLine("Введите часы (set)");
+            if (int.TryParse(Console.ReadLine(), out int hours))
+            {
+                try
+                {
+                    time.Hours = hours;
+                }
+                catch (TimeHourException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+    }
+}
